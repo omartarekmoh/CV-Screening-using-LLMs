42cd2ef519125f06470a6c8fcce9fc5417a3fda5
Omar Tarek <omartarekmoh1@gmail.com>
2025-04-27 20:48:10 +0300

    edit config

---
+++ src/helpers/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    APP_NAME: str
+    APP_VERSION: str
+    GROQ_API_KEY: str
+    
+    
+    GROQ_API_KEY: str
+
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    
+    FILE_DEFAULT_CHUNK_SIZE: int
+    
+    class Config:
+        env_file = ".env"
+        
+
+def get_settings():
+    return Settings()
