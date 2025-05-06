1e76bc4a8660154618fcb5a1ff3625278da4ff9a
Omar Tarek <omartarekmoh1@gmail.com>
2025-05-06 04:34:39 +0300

    edited interface to be one for generational models and one for embedding model

--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -1,24 +1,40 @@
 from abc import ABC, abstractmethod
 
-class LLMInterface(ABC):
-    
+class TextGenerationInterface(ABC):
+
     @abstractmethod
     def set_generation_model(self, model_id: str):
+        """
+        Set the model to be used for text generation.
+        """
         pass
     
     @abstractmethod
-    def set_embedding_model(self, model_id: str, embedding_size: int):
-        pass
-    
-    @abstractmethod
-    def generate_text(self, prompt: str, chat_history:list=[], max_output_tokens: int=None, temperature: float=None):
-        pass
-    
-    @abstractmethod
-    def embed_text(self, text: str, document_type: str =None):
+    def generate_text(self, prompt: str, chat_history: list = [], max_output_tokens: int = None, temperature: float = None):
+        """
+        Generate text based on the provided prompt and chat history.
+        """
         pass
     
     @abstractmethod
     def construct_prompt(self, prompt: str, role: str):
+        """
+        Construct a prompt to send to the model.
+        """
+        pass
+
+class EmbeddingInterface(ABC):
+    
+    @abstractmethod
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        """
+        Set the model to be used for generating text embeddings.
+        """
         pass
     
+    @abstractmethod
+    def embed_text(self, text: str, document_type: str = None):
+        """
+        Embed the input text and return its vector representation.
+        """
+        pass
